a228f41c960ed586b17f267dee0624dd527fb146
Jonathan D. Baker <jonathan@ardent.tech>
2026-01-29 10:08:25 -0700

    move tim1 time base code to stm32_tim

--- src/stm32_tim.c
+++ src/stm32_tim.c
@@ -3,6 +3,8 @@
 //
 #include "stm32_tim.h"
 
+TIM_HandleTypeDef htim1;
+
 void tim3_init(TIM_HandleTypeDef* htim3, void (*Error_Handler)(void)) {
     TIM_MasterConfigTypeDef sMasterConfig = {0};
     TIM_OC_InitTypeDef sConfigOC = {0};
@@ -40,46 +42,22 @@ void tim3_init(TIM_HandleTypeDef* htim3, void (*Error_Handler)(void)) {
     HAL_TIM_MspPostInit(htim3);
 }
 
-void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* htim_pwm)
-{
-    if(htim_pwm->Instance==TIM3)
-    {
-        /* USER CODE BEGIN TIM3_MspDeInit 0 */
-
-        /* USER CODE END TIM3_MspDeInit 0 */
-        /* Peripheral clock disable */
+void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* htim_pwm) {
+    if (htim_pwm->Instance==TIM3) {
         __HAL_RCC_TIM3_CLK_DISABLE();
-        /* USER CODE BEGIN TIM3_MspDeInit 1 */
-
-        /* USER CODE END TIM3_MspDeInit 1 */
     }
-
 }
 
-void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
-{
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim) {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
-    if(htim->Instance==TIM3)
-    {
-        /* USER CODE BEGIN TIM3_MspPostInit 0 */
-
-        /* USER CODE END TIM3_MspPostInit 0 */
-
+    if (htim->Instance==TIM3) {
         __HAL_RCC_GPIOA_CLK_ENABLE();
-        /**TIM3 GPIO Configuration
-    PA6     ------> TIM3_CH1
-    PA7     ------> TIM3_CH2
-         */
         GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF1_TIM3;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-        /* USER CODE BEGIN TIM3_MspPostInit 1 */
-
-        /* USER CODE END TIM3_MspPostInit 1 */
     }
 }
 
@@ -87,4 +65,73 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_pwm) {
     if (htim_pwm->Instance==TIM3) {
         __HAL_RCC_TIM3_CLK_ENABLE();
     }
+}
+
+void HAL_SuspendTick(void) {
+    __HAL_TIM_DISABLE_IT(&htim1, TIM_IT_UPDATE);
+}
+
+void HAL_ResumeTick(void) {
+    __HAL_TIM_ENABLE_IT(&htim1, TIM_IT_UPDATE);
+}
+
+/**
+ * @brief  This function configures the TIM1 as a time base source.
+ *         The time source is configured  to have 1ms time base with a dedicated
+ *         Tick interrupt priority.
+ * @note   This function is called  automatically at the beginning of program after
+ *         reset by HAL_Init() or at any time when clock is configured, by HAL_RCC_ClockConfig().
+ * @param  TickPriority: Tick interrupt priority.
+ * @retval HAL status
+ */
+HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority) {
+    RCC_ClkInitTypeDef    clkconfig;
+    uint32_t              uwTimclock = 0;
+    uint32_t              uwPrescalerValue = 0;
+    uint32_t              pFLatency;
+
+    /*Configure the TIM1 IRQ priority */
+    if (TickPriority < (1UL << __NVIC_PRIO_BITS)) {
+        HAL_NVIC_SetPriority(TIM1_BRK_UP_TRG_COM_IRQn, TickPriority ,0);
+
+        /* Enable the TIM1 global Interrupt */
+        HAL_NVIC_EnableIRQ(TIM1_BRK_UP_TRG_COM_IRQn);
+        uwTickPrio = TickPriority;
+    } else {
+        return HAL_ERROR;
+    }
+
+    /* Enable TIM1 clock */
+    __HAL_RCC_TIM1_CLK_ENABLE();
+
+    /* Get clock configuration */
+    HAL_RCC_GetClockConfig(&clkconfig, &pFLatency);
+
+    /* Compute TIM1 clock */
+    uwTimclock = HAL_RCC_GetPCLK1Freq();
+
+    /* Compute the prescaler value to have TIM1 counter clock equal to 1MHz */
+    uwPrescalerValue = (uint32_t) ((uwTimclock / 1000000U) - 1U);
+
+    /* Initialize TIM1 */
+    htim1.Instance = TIM1;
+
+    /* Initialize TIMx peripheral as follow:
+     * Period = [(TIM1CLK/1000) - 1]. to have a (1/1000) s time base.
+     * Prescaler = (uwTimclock/1000000 - 1) to have a 1MHz counter clock.
+     * ClockDivision = 0
+     * Counter direction = Up
+     */
+    htim1.Init.Period = (1000000U / 1000U) - 1U;
+    htim1.Init.Prescaler = uwPrescalerValue;
+    htim1.Init.ClockDivision = 0;
+    htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+
+    if(HAL_TIM_Base_Init(&htim1) == HAL_OK) {
+        /* Start the TIM time Base generation in interrupt mode */
+        return HAL_TIM_Base_Start_IT(&htim1);
+    }
+
+    /* Return function status */
+    return HAL_ERROR;
 }
